ffc6a244950380ac5a9723991bb26357cdf7ebed
Alina <55455721+agonzalez22@users.noreply.github.com>
2024-10-06 01:19:48 -0400

    added landing pages

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,9 +1,19 @@
-import App from './App.jsx'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import App from "./components/App.jsx";
+import "./index.css";
 
-function ViewList() {
-    return(
+function Landing() {
+  return (
+    <Router>
+      <div className="app-container">
         <App />
-    );
+        <div className="test">
+          <p>POOP</p>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
-export default ViewList;
+export default Landing;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App.jsx'
+import Landing from './Landing.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Landing/>
   </StrictMode>,
 )
 
